9c4e70890800d3da89907301dfde4c8a469a9c3f
Kolja Esders <kolja.esders@gmail.com>
2017-12-03 01:37:59 +0100

    Desig updates

--- src/pages/configurate-menu/configurate-menu.ts
+++ src/pages/configurate-menu/configurate-menu.ts
@@ -17,7 +17,7 @@ export class ConfigurateMenuPage {
   isRecording = false;
   response = '';
   keyword ='';
-  currentImg= 'assets/img/spaetzle.jpg';
+  currentImg= 'assets/img/pizza.jpg';
   meal = ["Roast beef", "Spaetzle", "Salad", "Sauce"];
 
   constructor(public navCtrl: NavController,
@@ -40,46 +40,22 @@ export class ConfigurateMenuPage {
   }
 
 
-identifyConfigurations(text: String){
-  console.log("identify config");
-  var words = text.split(" ");
-  var individualConfig
-  console.log(words)
+  identifyConfigurations(text: String){
+    console.log("identify config");
+    var words = text.split(" ");
+    var individualConfig
+    console.log(words)
 
-  for (var i = 0; i < words.length; i += 1) {
+    for (var i = 0; i < words.length; i += 1) {
 
-    if (words[i] == "avocado"){
-      individualConfig = "avocado";
+      if (words[i] == "avocado"){
+        individualConfig = "avocado";
+      }
+
     }
-
+    console.log("individualConfig")
+    return individualConfig
   }
-  console.log("individualConfig")
-  return individualConfig
-}
-
-/*
-  speechInput() {
-    if (this.loading) {
-      return;
-    }
-    this.loading = true;
-
-    let options = {
-      showPopup: false,  // Android only
-      showPartial: true // iOS only
-    };
-    this.speechRecognition.startListening(options).subscribe(matches => {
-      this.cd.detectChanges();
-      this.matches = matches;
-      console.log(matches);
-
-    }, errors => {
-      console.log(errors);
-    });
-    this.isRecording = true;
-  }
-*/
-
 
   startListening() {
       let options = {
@@ -115,11 +91,14 @@ identifyConfigurations(text: String){
     this.changeMeal("Avocado")
   }
 
-
   changeMeal(word){
     console.log("changeMeal")
     this.currentImg = 'assets/img/spaetzle_mit_Braten.jpg';
     this.meal.push(word)
+  }
+
+  order() {
+    this.navCtrl.push("OrderConfirmationPage");
   }
 
   ionViewDidLoad() {
